150e57e9700fe2e265d780078b38a3c3588ba435
Brittanie Barrett <britt.barrett302@gmail.com>
2020-09-03 17:27:16 -0700

    working on authentication

--- models/User.js
+++ models/User.js
@@ -2,4 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const User = new Schema({});
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  fullName: String,
+});
+
+UserSchema.methods.setFullName = function () {
+  this.fullName = `${this.firstName} ${this.lastName}`;
+
+  return this.fullName;
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
